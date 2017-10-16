0c721f0aa9ee729101b12cf5976db73ac658c3b5
kkrzeminski <krzysztof.krzeminski@gmail.com>
2017-10-16 10:47:54 +0200

    Use gradle wrapper 4.2.1; JAVA8; remove <<; gradle 5.0 compatitble

--- src/main/groovy/pl/ivmx/gradle/plugins/duplicatefinder/DuplicateFinderPlugin.groovy
+++ src/main/groovy/pl/ivmx/gradle/plugins/duplicatefinder/DuplicateFinderPlugin.groovy
@@ -3,6 +3,7 @@ package pl.ivmx.gradle.plugins.duplicatefinder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskExecutionException
+import org.gradle.tooling.model.Task
 
 import java.util.zip.ZipEntry
 import java.util.zip.ZipFile
@@ -11,37 +12,35 @@ class DuplicateFinderPlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create("duplicateFinder", DuplicateFinderPluginExtension)
 
-        def duplicateFinder = project.task('duplicateFinder') << { task ->
-            def Map<String, List<String>> exeptions = project.duplicateFinder.exceptions
-            def configurations = project.duplicateFinder.configs != null ? project.duplicateFinder.configs : project.configurations
-            configurations.each { configuration ->
-                def duplicates = configuration
-                        .collect { findClassesInJar(it) }
-                        .flatten()
-                        .groupBy { it.name }
-                        .findAll { it.value.size() > 1 }
-                        .findAll { !(exeptions.containsKey(it.key) && exeptions.get(it.key).containsAll(it.value.filename)) }
-                        .sort { it.key }
-                if (duplicates.size() > 0) {
-                    throw new TaskExecutionException(task,
-                            new Exception(configuration.name
-                                    + " ["
-                                    + duplicates.inject([]) { res, en -> res << "'$en.key' : ['" + en.value.filename.join("', '") + "']" }
-                                    .join(",\n") + "]"))
+        def duplicateFinder = project.task('duplicateFinder', {
+            doLast { task ->
+                Map<String, List<String>> exceptions = project.duplicateFinder.exceptions
+                def configurations = project.duplicateFinder.configs != null ? project.duplicateFinder.configs : project.configurations
+                configurations.findAll { !(it.name in ['apiElements']) }.each { configuration ->
+                    def duplicates = configuration
+                            .collect { findClassesInJar(it) }
+                            .flatten()
+                            .groupBy { it.name }
+                            .findAll { it.value.size() > 1 }
+                            .findAll { !(exceptions.containsKey(it.key) && exceptions.get(it.key).containsAll(it.value.filename)) }
+                            .sort { it.key }
+                    if (duplicates.size() > 0) {
+                        def duplicatedFiles = duplicates.inject([]) { res, en -> res << "'$en.key' : ['" + en.value.filename.join("', '") + "']" }.join(",\n")
+                        throw new TaskExecutionException(task, new Exception("$configuration.name: [$duplicatedFiles]".toString()))
+                    }
+                    println "configuration: " + configuration.name + " is OK"
                 }
-                println "configuration: " + configuration.name + " is OK"
             }
-        }
+        })
         duplicateFinder.description = 'Find class duplicates in given configurations'
         duplicateFinder.group = 'Verification'
     }
 
-    def findClassesInJar(File jar) {
+    static def findClassesInJar(File jar) {
         new ZipFile(jar).entries()
-                .findAll { it.name.endsWith('.class') }
-                .collect { new ClassInsideJar(it, jar.name) }
+                .findAll { ZipEntry it -> it.name.endsWith('.class') }
+                .collect { ZipEntry it -> new ClassInsideJar(it, jar.name) }
     }
-
 
 }
 
